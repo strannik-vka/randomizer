9a631f19a7bb790592852ff3c27748028bfc0beb
strannik-vka <canbka13@gmail.com>
2024-04-10 17:29:57 +0300

    fix

--- src/features/channels/subscribe/ui/SubscribeButton.jsx
+++ src/features/channels/subscribe/ui/SubscribeButton.jsx
@@ -1,12 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 
 const SubscribeButton = (props) => {
+    const [onClick, setOnClick] = useState(false);
 
     return (
-        props.isSubscribed ? (
+        (props.isSubscribed || onClick) ? (
             <button className="btn btn-sm btn-completed">Подписаны</button>
         ) : (
-            <a href={props.channel_link} target="_blank" className="btn btn-sm btn-primary">Подписаться</a>
+            <a onClick={() => setOnClick(true)} href={props.channel_link} target="_blank" className="btn btn-sm btn-primary">Подписаться</a>
         )
     )
 }
